cd34ce438941d66cba8a41c0833646845377695a
JairAntonio22 <jair.loranca@gmail.com>
2026-06-06 14:34:55 -0600

    Added support for number, char, and string to printf

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,5 +1,17 @@
 #include <kernel/printf.h>
 
 void kmain() {
-	printf("Hello, World!");
+	printf("Hello, World!\n");
+	printf("\n");
+
+	printf("Hello %x\n", 0);
+	printf("Hello %x\n", 5);
+	printf("Hello %x\n", 10);
+	printf("Hello %x\n", 20);
+	printf("\n");
+
+	printf("Hello %c\n", 'a');
+	printf("\n");
+
+	printf("Hello %s\n", "string");
 }
--- src/kernel/printf.c
+++ src/kernel/printf.c
@@ -3,29 +3,72 @@
 #include <lib/base.h>
 #include <stdarg.h>
 
+void print_number(uint64_t n);
+
 void printf(char *fmt, ...) {
-	size_t argc = 0;
-
-	for (size_t i = 0; fmt[i]; i++) {
-		argc += fmt[i] == '%';
-	}
-
-	if (!argc) {
-		uart_write_str(fmt);
-		uart_write_char('\n');
-		return;
-	}
-
 	va_list args;
 	va_start(args, fmt);
 
-	for (size_t i = 0; i < argc; i++) {
-		char *msg = va_arg(args, char*);
-		uart_write_str(msg);
+	while (*fmt) {
+		if (*fmt != '%') {
+			uart_write_char(*fmt++);
+			continue;
+		}
+
+		char argtype = fmt[1];
+
+		switch (argtype) {
+			case '\0': {
+			} break;
+
+			case 'x': {
+				uint64_t arg = va_arg(args, uint64_t);
+				print_number(arg);
+				fmt += 2;
+			} break;
+
+			case 'c': {
+				uint64_t arg = va_arg(args, uint64_t);
+				uart_write_char(arg);
+				fmt += 2;
+			} break;
+
+			case 's': {
+				char *arg = va_arg(args, char*);
+				uart_write_str(arg);
+				fmt += 2;
+			} break;
+
+			default: {
+				uint64_t arg = va_arg(args, uint64_t);
+				uart_write_char(arg);
+				fmt++;
+			} break;
+				
+		}
 	}
 
-	uart_write_char('\n');
 	va_end(args);
+}
+
+void print_number(uint64_t n) {
+	uart_write_str("0x");
+
+	uint64_t offset = 60;
+
+	for (int i = 0; i < 16; i++) {
+		uint8_t byte = (n & (0xf << offset)) >> offset;
+
+		if (byte < 10) {
+			uart_write_char(byte + '0');
+
+		} else {
+			uart_write_char((byte - 10) + 'a');
+		}
+
+		offset -= 4;
+	}
+
 }
 
 void panic(char *msg) {
